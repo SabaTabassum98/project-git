ce11dcb9a78f2cf5e09b699c67a0f56a645732e2
SabaTabassum98 <khalidzeeba@gmail.com>
2022-02-05 23:06:38 +0530

    Modified two files

--- src/pack/HelloBrother.java
+++ src/pack/HelloBrother.java
@@ -4,6 +4,7 @@ public class HelloBrother {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Brother");
+		System.out.println("Hi Brother !!");
 
 	}
 
--- src/pack1/HelloMr.java
+++ src/pack1/HelloMr.java
@@ -4,6 +4,7 @@ public class HelloMr {
 
 	public static void main(String[] args) {
 		System.out.println("Testing MR.");
+		System.out.println("Adding code in same file ");
 	}
 
 }
