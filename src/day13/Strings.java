282a23c87c8335c028596967c269d3204e0b0460
SabaTabassum98 <khalidzeeba@gmail.com>
2022-02-25 20:20:29 +0530

    1st

---
+++ src/day13/Strings.java
@@ -0,0 +1,17 @@
+package day13;
+
+public class Strings {
+	
+		  public static void main(String[] args) {
+		    
+		  
+		    String first = "{saba.";
+		    String second = "hyd.";
+		    String third = "india}";
+
+		 
+		    System.out.print(first);   
+		    System.out.print(second);  
+		    System.out.print(third);  
+		  }
+	}
